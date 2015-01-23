dda2bcbe878e72629856bcca11a52dd1ef584875
David G Chaves <davidgchaves@gmail.com>
2015-01-23 17:53:09 +0100

    Create example movies in the seeds.rb file to populate the dev-db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Movie.create [
+  {
+    title: "Winter Sleep",
+    rating: "PG",
+    total_gross: 318412101.00,
+    description: "If only Bergman were Turkish",
+    released_on: "2014-06-13"
+  },
+  {
+    title: "Leviathan",
+    rating: "R",
+    total_gross: 134218018.00,
+    description: "Nowadays Tarkovsky in a Russian coastal town",
+    released_on: "2015-02-05"
+  },
+  {
+    title: "The Salt of the Earth",
+    rating: "PG-13",
+    total_gross: 403706375.00,
+    description: "Very compelling doc about life, love, loss, despair and redemption",
+    released_on: "2015-03-27"
+  },
+  {
+    title: "Catwoman",
+    rating: "PG-13",
+    total_gross: 40200000.00,
+    description: "Patience Philips has a more than respectable career as a graphic designer",
+    released_on: "2004-07-23"
+  }
+]
