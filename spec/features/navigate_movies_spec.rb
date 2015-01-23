3932aa97a79a758e0d78cf0fd941fff5a712d98e
David G Chaves <davidgchaves@gmail.com>
2015-01-23 12:58:44 +0100

    RED: Navigating movies from the listing page to the detail page

--- spec/features/navigate_movies_spec.rb
+++ spec/features/navigate_movies_spec.rb
@@ -13,4 +13,17 @@ describe "Navigating movies" do
 
     expect(current_path).to eq movies_path
   end
+
+  it "allows navigation from the listing page to the detail page" do
+    movie = Movie.create title: "Winter Sleep",
+                         rating: "PG",
+                         total_gross: 318412101.00,
+                         description: "If only Bergman were Turkish",
+                         released_on: "2014-06-13"
+    visit movies_url
+
+    click_link movie.title
+
+    expect(current_path).to eq movie_path(movie)
+  end
 end
