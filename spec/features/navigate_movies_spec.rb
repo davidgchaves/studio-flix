c4769f3536fe1f22f0b05eb49393f13e47308808
David G Chaves <davidgchaves@gmail.com>
2015-01-26 10:03:49 +0100

    RED: Navigating movies from the create page to the detail page

--- spec/features/navigate_movies_spec.rb
+++ spec/features/navigate_movies_spec.rb
@@ -44,4 +44,18 @@ describe "Navigating movies" do
 
     expect(current_path).to eq new_movie_path
   end
+
+  it "allows navigation from the create page to the detail page" do
+    visit movies_url
+    click_link "Add New Movie"
+    fill_in "Title", with: "New Movie Title"
+    fill_in "Description", with: "Superheroes saving the world from villains"
+    fill_in "Rating", with: "PG-13"
+    fill_in "Total gross", with: "75000000"
+    select (Time.now.year - 1).to_s, :from => "movie_released_on_1i"
+
+    click_button "Create Movie"
+
+    expect(current_path).to eq movie_path(Movie.last)
+  end
 end
