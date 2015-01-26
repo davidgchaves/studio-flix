f2118a96b7e38e7b67f770fca8f59e446e47341e
David G Chaves <davidgchaves@gmail.com>
2015-01-26 10:09:47 +0100

    RED: Creating a new movie shows the new movie's details

--- spec/features/create_movie_spec.rb
+++ spec/features/create_movie_spec.rb
@@ -9,4 +9,18 @@ describe "Creating a new movie" do
     expect(find_field("Title").value).to eq nil
     expect(find_field("Description").value).to eq ""
   end
+
+  it "shows the new movie's details" do
+    visit movies_url
+    click_link "Add New Movie"
+    fill_in "Title", with: "New Movie Title"
+    fill_in "Description", with: "Superheroes saving the world from villains"
+    fill_in "Rating", with: "PG-13"
+    fill_in "Total gross", with: "75000000"
+    select (Time.now.year - 1).to_s, :from => "movie_released_on_1i"
+
+    click_button "Create Movie"
+
+    expect(page).to have_text "New Movie Title"
+  end
 end
