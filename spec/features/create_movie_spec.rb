2ffaf80b45de7404cfba025e797ba5c45c603d45
David G Chaves <davidgchaves@gmail.com>
2015-01-27 10:18:52 +0100

    Clean up "Creating a new movie" scenario

--- spec/features/create_movie_spec.rb
+++ spec/features/create_movie_spec.rb
@@ -1,26 +1,37 @@
 require "rails_helper"
 
 describe "Creating a new movie" do
-  it "shows the form to create movies" do
+  before do
     visit movies_url
-
     click_link "Add New Movie"
+  end
 
+  it "goes to the create page" do
+    expect(current_path).to eq new_movie_path
+  end
+
+  it "shows the form to create movies" do
     expect(find_field("Title").value).to eq nil
     expect(find_field("Description").value).to eq ""
   end
 
-  it "shows the new movie's details" do
-    visit movies_url
-    click_link "Add New Movie"
-    fill_in "Title", with: "New Movie Title"
-    fill_in "Description", with: "Superheroes saving the world from villains"
-    fill_in "Rating", with: "PG-13"
-    fill_in "Total gross", with: "75000000"
-    select (Time.now.year - 1).to_s, :from => "movie_released_on_1i"
+  context "when done" do
+    before do
+      fill_in "Title", with: "New Movie Title"
+      fill_in "Description", with: "Superheroes saving the world from villains"
+      fill_in "Rating", with: "PG-13"
+      fill_in "Total gross", with: "75000000"
+      select (Time.now.year - 1).to_s, :from => "movie_released_on_1i"
 
-    click_button "Create Movie"
+      click_button "Create Movie"
+    end
 
-    expect(page).to have_text "New Movie Title"
+    it "redirects to the detail page" do
+      expect(current_path).to eq movie_path(Movie.last)
+    end
+
+    it "shows the new movie's details" do
+      expect(page).to have_text "New Movie Title"
+    end
   end
 end
--- spec/features/navigate_movies_spec.rb
+++ spec/features/navigate_movies_spec.rb
@@ -4,28 +4,6 @@ require "support/attributes"
 describe "Navigating movies" do
   let!(:movie) { movie = Movie.create movie_attributes }
 
-  it "allows navigation from the listing page to the create page" do
-    visit movies_url
-
-    click_link "Add New Movie"
-
-    expect(current_path).to eq new_movie_path
-  end
-
-  it "allows navigation from the create page to the detail page" do
-    visit movies_url
-    click_link "Add New Movie"
-    fill_in "Title", with: "New Movie Title"
-    fill_in "Description", with: "Superheroes saving the world from villains"
-    fill_in "Rating", with: "PG-13"
-    fill_in "Total gross", with: "75000000"
-    select (Time.now.year - 1).to_s, :from => "movie_released_on_1i"
-
-    click_button "Create Movie"
-
-    expect(current_path).to eq movie_path(Movie.last)
-  end
-
   it "allows navigation from the details page to the listing page destroying a movie" do
     visit movie_url(movie)
 
