d933d53bf07e4722836b1940ec8f5f5a280da4f8
David G Chaves <davidgchaves@gmail.com>
2015-01-27 10:21:19 +0100

    Clean up "Deleting a movie" scenario

--- spec/features/delete_movie_spec.rb
+++ spec/features/delete_movie_spec.rb
@@ -2,12 +2,18 @@ require "rails_helper"
 require "support/attributes"
 
 describe "Deleting a movie" do
-  it "shows the movie listing without the deleted movie" do
-    movie = Movie.create movie_attributes
+  let!(:movie) { Movie.create movie_attributes }
+
+  before do
     visit movie_url(movie)
-
     click_link "Delete"
+  end
 
+  it "redirects to the listing page destroying a movie" do
+    expect(current_path).to eq movies_path
+  end
+
+  it "shows the movie listing without the deleted movie" do
     expect(page).not_to have_text movie.title
     expect(page).not_to have_text movie.description
   end
--- spec/features/navigate_movies_spec.rb
+++ spec/features/navigate_movies_spec.rb
@@ -3,12 +3,4 @@ require "support/attributes"
 
 describe "Navigating movies" do
   let!(:movie) { movie = Movie.create movie_attributes }
-
-  it "allows navigation from the details page to the listing page destroying a movie" do
-    visit movie_url(movie)
-
-    click_link "Delete"
-
-    expect(current_path).to eq movies_path
-  end
 end
