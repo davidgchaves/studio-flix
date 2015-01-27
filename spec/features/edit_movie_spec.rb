919dde2a73b7dfc0961c64bee4f0239d0bebbfaa
David G Chaves <davidgchaves@gmail.com>
2015-01-27 10:14:30 +0100

    Clean up "Editing a movie" scenario

--- spec/features/edit_movie_spec.rb
+++ spec/features/edit_movie_spec.rb
@@ -2,23 +2,33 @@ require "rails_helper"
 require "support/attributes"
 
 describe "Editing a movie" do
-  it "shows the form to edit movies" do
-    movie = Movie.create movie_attributes
-    visit movie_path(movie)
+  let!(:movie) { Movie.create movie_attributes }
 
+  before do
+    visit movie_url(movie)
     click_link "Edit"
+  end
 
+  it "goes to the edit page" do
+    expect(current_path).to eq edit_movie_path(movie)
+  end
+
+  it "shows the form to edit movies" do
     expect(find_field('Title').value).to eq movie.title
   end
 
-  it "shows the movie's updated details" do
-    movie = Movie.create movie_attributes
-    visit movie_path(movie)
-    click_link "Edit"
-    fill_in "Title" , with: "Updated Movie Title"
+  context "when done" do
+    before do
+      fill_in "Title" , with: "Updated Movie Title"
+      click_button "Update Movie"
+    end
 
-    click_button "Update Movie"
+    it "redirects to the detail page" do
+      expect(current_path).to eq movie_path(movie)
+    end
 
-    expect(page).to have_text "Updated Movie Title"
+    it "shows the movie's updated details" do
+      expect(page).to have_text "Updated Movie Title"
+    end
   end
 end
--- spec/features/navigate_movies_spec.rb
+++ spec/features/navigate_movies_spec.rb
@@ -4,23 +4,6 @@ require "support/attributes"
 describe "Navigating movies" do
   let!(:movie) { movie = Movie.create movie_attributes }
 
-  it "allows navigation from the detail page to the edit page" do
-    visit movie_url(movie)
-
-    click_link "Edit"
-
-    expect(current_path).to eq edit_movie_path(movie)
-  end
-
-  it "allows navigation from the edit page to the detail page" do
-    visit movie_url(movie)
-    click_link "Edit"
-
-    click_button "Update Movie"
-
-    expect(current_path).to eq movie_path(movie)
-  end
-
   it "allows navigation from the listing page to the create page" do
     visit movies_url
 
