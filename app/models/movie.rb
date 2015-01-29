acf8ac741bbfb2e86ad1b02b3bf464d1e97e04f0
David G Chaves <davidgchaves@gmail.com>
2015-01-29 13:37:31 +0100

    GREEN: Hit movies (at least $300M gross) in descending order

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,4 +6,8 @@ class Movie < ActiveRecord::Base
   def self.released
     where("released_on <= ?", Time.now).order released_on: :desc
   end
+
+  def self.hits
+    where("total_gross >= ?", 300000000).order total_gross: :desc
+  end
 end
