0d4adbd4ceb67bdc00e17cb4da40ba96e725dc2d
David G Chaves <davidgchaves@gmail.com>
2015-01-29 13:35:03 +0100

    RED: Hit movies (at least $300M gross) in descending order

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -39,4 +39,24 @@ describe "A movie" do
 
     expect(Movie.released).to eq [movie3, movie2, movie1]
   end
+
+  it "is a hit when the total gross is at least $300M" do
+    movie = Movie.create movie_attributes(total_gross: 400000000)
+
+    expect(Movie.hits).to include movie
+  end
+
+  it "is not a hit when the total gross is less than $300M" do
+    movie = Movie.create movie_attributes(total_gross: 200000000)
+
+    expect(Movie.hits).not_to include movie
+  end
+
+  it "returns hits movies ordered with the highest grossing movie first" do
+    movie1 = Movie.create movie_attributes(total_gross: 300000000)
+    movie2 = Movie.create movie_attributes(total_gross: 400000000)
+    movie3 = Movie.create movie_attributes(total_gross: 500000000)
+
+    expect(Movie.hits).to eq [movie3, movie2, movie1]
+  end
 end
