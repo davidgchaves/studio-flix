2c48f0e2b9182bf378405eaf65a8ee6fc28ae89e
David G Chaves <davidgchaves@gmail.com>
2015-02-01 18:24:06 +0100

    RED: The sidebar allows direct navigation to the add new movie page

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -2,12 +2,20 @@ require "rails_helper"
 require "support/attributes"
 
 describe "The sidebar" do
-  it "allows direct navigation to the listing page" do
+  before do
     movie = Movie.create movie_attributes
     visit movie_url(movie)
+  end
 
+  it "allows direct navigation to the listing page" do
     click_link "All Movies"
 
     expect(current_path).to eq movies_path
+  end
+
+  it "allows direct navigation to the add new movie page" do
+    click_link "Add New Movie"
+
+    expect(current_path).to eq new_movie_path
   end
 end
