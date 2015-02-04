f1f30547caa054eff098517508421ebc8f61c666
David G Chaves <davidgchaves@gmail.com>
2015-02-04 19:37:52 +0100

    GREEN: A movie only accepts a predefined list of valid ratings

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,10 @@
 class Movie < ActiveRecord::Base
+  RATINGS = %w(G PG PG-13 R NC-17)
+
   validates :title, :released_on, :duration, presence: true
   validates :description, length: { minimum: 25 }
   validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+  validates :rating, inclusion: { in: RATINGS }
   validates :image_file_name, allow_blank: true, format: {
     with: /\w+\.(jpg|png|gif)\z/i,
     message: "must reference a GIF, JPG or PNG image" }
