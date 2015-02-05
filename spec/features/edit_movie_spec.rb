4c6ebcf9907f7b8cf877e97416ae01058adbd5f4
David G Chaves <davidgchaves@gmail.com>
2015-02-05 19:00:27 +0100

    RED: Editing a movie on failure shows info about what was wrong

--- spec/features/edit_movie_spec.rb
+++ spec/features/edit_movie_spec.rb
@@ -17,7 +17,7 @@ describe "Editing a movie" do
     expect(find_field('Title').value).to eq movie.title
   end
 
-  context "when done" do
+  context "on success" do
     before do
       fill_in "Title" , with: "Updated Movie Title"
       click_button "Update Movie"
@@ -29,6 +29,28 @@ describe "Editing a movie" do
 
     it "shows the movie's updated details" do
       expect(page).to have_text "Updated Movie Title"
+    end
+  end
+
+  context "on failure" do
+    before do
+      fill_in "Title", with: ""
+      fill_in "Description", with: "Superheroes saving the world from villains"
+
+      click_button "Update Movie"
+    end
+
+    it "renders again the edit template" do
+      expect(current_path).to eq movie_path(movie)
+      expect(page).to have_text "Editing"
+    end
+
+    it "shows info about what was wrong" do
+      expect(page).to have_text "correct the following"
+    end
+
+    it "preserves the movie info previously entered" do
+      expect(page).to have_text "Superheroes saving the world from villains"
     end
   end
 end
