5f759eab8e0a0992855912bce586150f777cf20d
David G Chaves <davidgchaves@gmail.com>
2015-02-07 11:23:07 +0100

    RED: The flash is never shown with an empty message

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -1,9 +1,13 @@
 require "rails_helper"
 
 describe "The flash" do
-  it "is not shown with an empty notice message" do
+  it "is never shown with an empty message" do
+    flash_types = %w[notice alert whatever]
+
     visit movies_url
 
-    expect(page).not_to have_css "p.flash.notice"
+    flash_types.each do |flash_type|
+      expect(page).not_to have_css "p.flash.#{flash_type}"
+    end
   end
 end
