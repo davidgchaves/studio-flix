931f7fc9bd1d348f3dc76c9e3d13bfb6f848cf73
David G Chaves <davidgchaves@gmail.com>
2015-02-09 12:57:19 +0100

    RED: A review only accepts star values of 1 to 5

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -25,5 +25,27 @@ describe "A review" do
 
     expect(invalid_review.errors[:comment].any?).to eq true
   end
+
+  it "rejects invalid star values with a custom error message" do
+    invalid_stars = [0, -1, 6, 10]
+    invalid_stars.each do |invalid_star|
+      invalid_review = Review.new stars: invalid_star
+
+      invalid_review.valid?
+
+      expect(invalid_review.errors[:stars].first).to eq "must be between 1 and 5"
+    end
+  end
+
+  it "accepts star values of 1 to 5" do
+    valid_stars = [1,2,3,4,5]
+    valid_stars.each do |valid_star|
+      valid_review = Review.new review_attributes(stars: valid_star)
+
+      valid_review.valid?
+
+      expect(valid_review.errors[:stars].any?).to eq false
+    end
+  end
 end
 
