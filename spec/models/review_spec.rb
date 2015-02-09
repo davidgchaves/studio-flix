57d8150fbdb3c623640dd7cac006e67ab79aa0cf
David G Chaves <davidgchaves@gmail.com>
2015-02-09 12:13:09 +0100

    RED: A review belongs to a movie

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
+require 'support/attributes'
 
-RSpec.describe Review, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "A review" do
+  it "belongs to a movie" do
+    movie = Movie.create movie_attributes
+
+    review = movie.reviews.new review_attributes
+
+    expect(review.movie).to eq movie
+  end
 end
+
