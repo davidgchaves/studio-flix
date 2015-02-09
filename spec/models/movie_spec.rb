e1f1d9603312c429c8005b707ab41691cdd18f2b
David G Chaves <davidgchaves@gmail.com>
2015-02-09 12:18:04 +0100

    RED: A movie has many reviews

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -2,6 +2,16 @@ require "rails_helper"
 require "support/attributes"
 
 describe "A movie" do
+  it "has many reviews" do
+    movie = Movie.create movie_attributes
+
+    review1 = movie.reviews.new review_attributes
+    review2 = movie.reviews.new review_attributes
+
+    expect(movie.reviews).to include review1
+    expect(movie.reviews).to include review2
+  end
+
   it "is a flop if the total gross is less than $50M" do
     flop_movie = Movie.new total_gross: 40000000.00
 
