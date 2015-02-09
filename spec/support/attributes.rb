f57cda1c123f178fad9f65ffe3175e03417e25a3
David G Chaves <davidgchaves@gmail.com>
2015-02-09 12:10:58 +0100

    Create a review_attributes helper method for easy spec'ing

--- spec/support/attributes.rb
+++ spec/support/attributes.rb
@@ -11,3 +11,12 @@ def movie_attributes(override = {})
     image_file_name: "wintersleep.jpg"
   }.merge(override)
 end
+
+def review_attributes(overrides = {})
+  {
+    name: "Roger Ebert",
+    stars: 3,
+    comment: "I laughed, I cried, I spilled my popcorn!"
+  }.merge(overrides)
+end
+
