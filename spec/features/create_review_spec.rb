20e1d29da62229e068ff0205fd6ae72b76f7fa42
David G Chaves <davidgchaves@gmail.com>
2015-02-10 12:50:57 +0100

    RED: Creating a new review for a movie...
    ...shows the form to create reviews

--- spec/features/create_review_spec.rb
+++ spec/features/create_review_spec.rb
@@ -2,12 +2,25 @@ require 'rails_helper'
 require 'support/attributes'
 
 describe "Creating a new review for a movie" do
-  it "goes to the create review page" do
-    movie = Movie.create movie_attributes
+  let(:movie) { Movie.create movie_attributes }
+
+  before do
     visit movie_url(movie)
 
     click_link "Write Review"
+  end
 
+  it "goes to the create review page" do
     expect(current_path).to eq new_movie_review_path(movie)
+  end
+
+  it "shows the form to create reviews" do
+    expect(page).to have_text movie.title
+    expect(page).to have_field "Name"
+    expect(page).to have_field "Comment"
+    expect(page).to have_field "Location"
+    expect(page).to have_select "Stars"
+    expect(page).to have_button "Post Review"
+    expect(page).to have_link "Cancel"
   end
 end
