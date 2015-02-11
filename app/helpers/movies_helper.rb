60b0f6cdde2cee6ab4b15bd2fde274c47bf5adbb
David G Chaves <davidgchaves@gmail.com>
2015-02-11 09:23:54 +0100

    GREEN: ...shows the average number of review stars

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,6 +3,14 @@ module MoviesHelper
     movie.flop? ? content_tag(:strong, "Flop!") : number_to_currency(movie.total_gross)
   end
 
+  def format_average_stars(movie)
+    if movie.reviews.empty?
+      content_tag :strong, "No Reviews"
+    else
+      pluralize number_with_precision(movie.average_stars, precision: 1), "star"
+    end
+  end
+
   def image_for(movie)
     movie.image_file_name.blank? ? image_tag("placeholder.png") : image_tag(movie.image_file_name)
   end
