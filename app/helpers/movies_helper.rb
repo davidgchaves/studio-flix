7b96d04ac7b8bb6ca187cbad8557fe8660a30bc8
David G Chaves <davidgchaves@gmail.com>
2015-02-11 11:59:43 +0100

    Refactor show movies page extracting out the recent reviews section

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -11,6 +11,10 @@ module MoviesHelper
     end
   end
 
+  def format_recent_reviews(movie)
+    render('layouts/recent_reviews_section') if movie.recent_reviews.any?
+  end
+
   def image_for(movie)
     movie.image_file_name.blank? ? image_tag("placeholder.png") : image_tag(movie.image_file_name)
   end
