d9db762e8113bda8eadc4bfa12bb80e104e98b8d
David G Chaves <davidgchaves@gmail.com>
2015-02-13 18:16:27 +0100

    GREEN: A non-blank user password has to match the password confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,5 +9,15 @@ describe User do
 
       expect(invalid_user.errors[:password].any?).to be_truthy
     end
+
+    context "when present" do
+      it "has to match the password confirmation" do
+        invalid_user = User.new password: "secret", password_confirmation: "nomatch"
+
+        invalid_user.valid?
+
+        expect(invalid_user.errors[:password_confirmation].any?).to be_truthy
+      end
+    end
   end
 end
