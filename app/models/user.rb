976201775de14e112f731c7ca3436de9b72c3740
David G Chaves <davidgchaves@gmail.com>
2015-02-13 19:35:36 +0100

    Use the email_validator instead of the custom regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false },
-                    format: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+                    email: true
   validates :password, length: { minimum: 10, allow_blank: true }
 end
