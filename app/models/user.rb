9808e84e000483f304d6b8e728af4159d132ae6d
David G Chaves <davidgchaves@gmail.com>
2015-02-13 18:39:12 +0100

    GREEN: A user email has to be unique and case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false }
 end
