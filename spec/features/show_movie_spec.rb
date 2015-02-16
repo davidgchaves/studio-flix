093d5ed9473d9cad4d8fdefe80b61dc758720b8e
David G Chaves <davidgchaves@gmail.com>
2015-02-16 13:21:28 +0100

    Refactor show movie spec (use let when possible)

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -2,9 +2,9 @@ require "rails_helper"
 require "support/attributes"
 
 describe "Viewing an individual movie" do
-  it "shows the movie's details" do
-    movie = Movie.create movie_attributes
+  let(:movie) { Movie.create movie_attributes }
 
+  it "shows the movie's details" do
     visit movie_url(movie)
 
     expect(page).to have_text movie.title
@@ -17,11 +17,8 @@ describe "Viewing an individual movie" do
   end
 
   context "when the total gross exceeds $50M" do
-    before(:example) do
-      movie = Movie.create movie_attributes(total_gross: 60000000.00)
-
-      visit movie_url(movie)
-    end
+    let(:movie) { Movie.create movie_attributes(total_gross: 60000000.00) }
+    before(:example) { visit movie_url(movie) }
 
     it "shows the total gross" do
       expect(page).to have_text "$60,000,000.00"
@@ -29,11 +26,8 @@ describe "Viewing an individual movie" do
   end
 
   context "when the total gross is less than $50M" do
-    before(:example) do
-      flop_movie = Movie.create movie_attributes(total_gross: 40000000.00)
-
-      visit movie_url(flop_movie)
-    end
+    let(:flop_movie) { Movie.create movie_attributes(total_gross: 40000000.00) }
+    before(:example) { visit movie_url(flop_movie) }
 
     it "shows 'Flop!'" do
       expect(page).to have_text "Flop!"
@@ -41,23 +35,17 @@ describe "Viewing an individual movie" do
   end
 
   context "when there's a poster associated to the movie" do
-    before(:example) do
-      @movie_with_poster = Movie.create movie_attributes(image_file_name: "wintersleep.jpg")
-
-      visit movie_url(@movie_with_poster)
-    end
+    let(:movie_with_poster) { Movie.create movie_attributes(image_file_name: "wintersleep.jpg") }
+    before(:example) { visit movie_url(movie_with_poster) }
 
     it "shows the movie's poster" do
-      expect(page).to have_selector "img[src$='#{@movie_with_poster.image_file_name}']"
+      expect(page).to have_selector "img[src$='#{movie_with_poster.image_file_name}']"
     end
   end
 
   context "when there's no poster associated to the movie" do
-    before(:example) do
-      movie_with_no_poster = Movie.create movie_attributes(image_file_name: "")
-
-      visit movie_url(movie_with_no_poster)
-    end
+    let(:movie_with_no_poster) { Movie.create movie_attributes(image_file_name: "") }
+    before(:example) { visit movie_url(movie_with_no_poster) }
 
     it "shows a default poster" do
       expect(page).to have_selector "img[src$='placeholder.png']"
@@ -65,31 +53,29 @@ describe "Viewing an individual movie" do
   end
 
   context "when there's at least a review" do
-    before(:example) do
-      movie = Movie.create movie_attributes
-      movie.reviews.create review_attributes(stars: 1)
-      @recent_review1 = movie.reviews.create review_attributes(stars: 4)
-      @recent_review2 = movie.reviews.create review_attributes(stars: 2)
-
-      visit movie_url(movie)
+    let(:movie) do
+      m = Movie.create movie_attributes
+      m.reviews.create review_attributes(stars: 1)
+      m
     end
+    let!(:recent_review1) { movie.reviews.create review_attributes(stars: 4) }
+    let!(:recent_review2) { movie.reviews.create review_attributes(stars: 2) }
+
+    before(:example) { visit movie_url(movie) }
 
     it "shows the average number of review stars" do
       expect(page).to have_text "2.3 stars"
     end
 
     it "shows the most-recently posted reviews" do
-      expect(page).to have_text "#{@recent_review1.stars} stars"
-      expect(page).to have_text "#{@recent_review2.stars} stars"
+      expect(page).to have_text "#{recent_review1.stars} stars"
+      expect(page).to have_text "#{recent_review2.stars} stars"
     end
   end
 
   context "when there's no reviews" do
-    before(:example) do
-      movie = Movie.create movie_attributes
-
-      visit movie_url(movie)
-    end
+    let(:movie) { Movie.create movie_attributes }
+    before(:example) { visit movie_url(movie) }
 
     it "shows a 'No Reviews' message" do
       expect(page).to have_text "No Reviews"
@@ -100,14 +86,20 @@ describe "Viewing an individual movie" do
     end
   end
 
-  it "allows navigation to its reviews" do
-    movie = Movie.create movie_attributes
-    movie.reviews.create review_attributes
-    movie.reviews.create review_attributes
-    visit movie_url(movie)
+  context "with reviews" do
+    let(:movie_with_2_reviews) do
+      m = Movie.create movie_attributes
+      m.reviews.create review_attributes
+      m.reviews.create review_attributes
+      m
+    end
 
-    click_link "2 reviews"
+    it "allows navigation to the listing reviews page" do
+      visit movie_url(movie_with_2_reviews)
 
-    expect(current_path).to eq movie_reviews_path(movie)
+      click_link "2 reviews"
+
+      expect(current_path).to eq movie_reviews_path(movie_with_2_reviews)
+    end
   end
 end
