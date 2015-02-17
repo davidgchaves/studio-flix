5a50e3edc682e8691bfb6da2cafca6b4707ba572
David G Chaves <davidgchaves@gmail.com>
2015-02-17 16:26:06 +0100

    Bundle install and config of shoulda-matchers gem

--- Gemfile
+++ Gemfile
@@ -56,6 +56,7 @@ end
 
 group :test do
   gem 'capybara', '~> 2.4.4'
+  gem 'shoulda-matchers', '~> 2.8.0', require: false
 end
 
 gem 'email_validator', '~> 1.5'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'shoulda/matchers'
 require 'capybara/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
